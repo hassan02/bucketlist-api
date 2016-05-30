c14d4368a00fc18091a93dada1cc80a24e0b50ce
andela-ydaniju <hassan.oyeboade@andela.com>
2016-05-30 03:09:33 +0100

    Added more test cases for Login and Register endpoints

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -7,6 +7,7 @@ from app.models import SQLAlchemy, User, BucketList, BucketListItem
 from app import app
 from app.helpers import *
 
+
 class TestBucketList(unittest.TestCase):
 
     def setUp(self):
@@ -27,11 +28,11 @@ class TestBucketList(unittest.TestCase):
         test_data = {'username': 'test_user', 'password': 'test_password'}
         self.register_request = self.app.post('/auth/register', data=test_data)
 
-    def login_with_valid_details(self):
+    def login_with_invalid_details(self):
         self.register_user()
-        test_data = {'username': 'test_user', 'password': 'test_password'}
+        test_data = {'username': 'andela', 'password': 'andela'}
         self.login_request = self.app.post('/auth/login', data=test_data)
-        
+
     def test_register_user(self):
         self.register_user()
         user = User.query.filter_by(username='test_user', password=hashlib.sha512(
@@ -39,11 +40,45 @@ class TestBucketList(unittest.TestCase):
         self.assertEqual(self.register_request.status_code, 201)
         self.assertIsNotNone(user)
         self.assertEqual(user.username, "test_user")
-        self.assertEqual(user.password, hashlib.sha512("test_password").hexdigest())
+        self.assertEqual(user.password, hashlib.sha512(
+            "test_password").hexdigest())
 
     def test_valid_user_login(self):
-        self.login_with_valid_details()
+        self.register_user()
+        self.login_request = self.app.post(
+            '/auth/login', data={'username': 'test_user', 'password': 'test_password'})
         self.assertEqual(self.login_request.status_code, 200)
-        
+
+    def test_login_with_invalid_username(self):
+        self.register_user()
+        self.login_request = self.app.post(
+            '/auth/login', data={'username': 'andela', 'password': 'test_password'})
+        self.assertEqual(self.login_request.status_code, 406)
+
+    def test_login_with_invalid_password(self):
+        self.register_user()
+        self.login_request = self.app.post(
+            '/auth/login', data={'username': 'test_user', 'password': 'andela'})
+        self.assertEqual(self.login_request.status_code, 406)
+
+    def test_login_with_blank_details(self):
+        self.register_user()
+        self.login_request = self.app.post(
+            '/auth/login', data={'username': '', 'password': 'andela'})
+        self.assertEqual(self.login_request.status_code, 406)
+
+    def test_register_with_existing_username(self):
+      self.register_user()
+      self.register_another_user = self.app.post('/auth/register', data={'username': 'test_user', 'password': 'andela'})
+      self.assertEqual(self.register_another_user.status_code, 406)
+
+    def test_register_with_blank_details(self):
+      self.register_request = self.app.post('/auth/register', data={'username': '', 'password': 'andela'})
+      self.assertEqual(self.register_request.status_code, 406)
+
+    
+
+
+
 if __name__ == '__main__':
     unittest.main()
