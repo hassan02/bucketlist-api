ec27f0eeedd258c76c5ad3ed4de90c7dead558df
andela-ydaniju <hassan.oyeboade@andela.com>
2016-05-30 03:37:01 +0100

    Change test database

--- config_test.py
+++ config_test.py
@@ -1,4 +1,4 @@
 import os
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'tests/test_bucketlist.sqlite')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'tests/bucketlist_test.sqlite')
 SECRET_KEY = 'secret'
