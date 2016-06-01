95b659de315074928f0bc3f346e1e1b42a167042
andela-ydaniju <hassan.oyeboade@andela.com>
2016-06-01 12:38:18 +0100

    Check test coverage

---
+++ manage.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from app import app
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
