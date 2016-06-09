b9b1ee72ca61ff96b1393d295850f8e5bb536ce2
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-06-09 12:57:20 +0100

    Change all request to response in tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -17,18 +17,18 @@ class TestLoginRegister(unittest.TestCase):
 
     def register_user(self):
         test_data = {'username': 'test_user', 'password': 'test_password'}
-        self.register_request = self.app.post('/api/v1/auth/register', data=test_data)
+        self.register_response = self.app.post('/api/v1/auth/register', data=test_data)
 
     def login_with_invalid_details(self):
         self.register_user()
         test_data = {'username': 'andela', 'password': 'andela'}
-        self.login_request = self.app.post('/api/v1/auth/login', data=test_data)
+        self.login_response = self.app.post('/api/v1/auth/login', data=test_data)
 
     def test_register_user(self):
         self.register_user()
         user = User.query.filter_by(username='test_user', password=hashlib.sha512(
             'test_password').hexdigest()).first()
-        self.assertEqual(self.register_request.status_code, 201)
+        self.assertEqual(self.register_response.status_code, 201)
         self.assertIsNotNone(user)
         self.assertEqual(user.username, 'test_user')
         self.assertEqual(user.password, hashlib.sha512(
@@ -36,27 +36,27 @@ class TestLoginRegister(unittest.TestCase):
 
     def test_valid_user_login(self):
         self.register_user()
-        self.login_request = self.app.post(
+        self.login_response = self.app.post(
             '/api/v1/auth/login', data={'username': 'test_user', 'password': 'test_password'})
-        self.assertEqual(self.login_request.status_code, 200)
+        self.assertEqual(self.login_response.status_code, 200)
 
     def test_login_with_invalid_username(self):
         self.register_user()
-        self.login_request = self.app.post(
+        self.login_response = self.app.post(
             '/api/v1/auth/login', data={'username': 'andela', 'password': 'test_password'})
-        self.assertEqual(self.login_request.status_code, 406)
+        self.assertEqual(self.login_response.status_code, 406)
 
     def test_login_with_invalid_password(self):
         self.register_user()
-        self.login_request = self.app.post(
+        self.login_response = self.app.post(
             '/api/v1/auth/login', data={'username': 'test_user', 'password': 'andela'})
-        self.assertEqual(self.login_request.status_code, 406)
+        self.assertEqual(self.login_response.status_code, 406)
 
     def test_login_with_blank_details(self):
         self.register_user()
-        self.login_request = self.app.post(
+        self.login_response = self.app.post(
             '/api/v1/auth/login', data={'username': '', 'password': 'andela'})
-        self.assertEqual(self.login_request.status_code, 406)
+        self.assertEqual(self.login_response.status_code, 406)
 
     def test_register_with_existing_username(self):
       self.register_user()
@@ -64,8 +64,8 @@ class TestLoginRegister(unittest.TestCase):
       self.assertEqual(self.register_another_user.status_code, 406)
 
     def test_register_with_blank_details(self):
-      self.register_request = self.app.post('/api/v1/auth/register', data={'username': '', 'password': 'andela'})
-      self.assertEqual(self.register_request.status_code, 406)
+      self.register_response = self.app.post('/api/v1/auth/register', data={'username': '', 'password': 'andela'})
+      self.assertEqual(self.register_response.status_code, 406)
 
 
 
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -19,25 +19,25 @@ class TestBucketList(unittest.TestCase):
         self.user_data = {'username': 'test_user', 'password': 'test_password'}
 
     def login(self):
-        self.register_request = self.app.post(
+        self.register_response = self.app.post(
             '/api/v1/auth/register', data=self.user_data)
-        self.assertEqual(self.register_request.status_code, 201)
-        self.login_request = self.app.post(
+        self.assertEqual(self.register_response.status_code, 201)
+        self.login_response = self.app.post(
             '/api/v1/auth/login', data=self.user_data)
-        self.assertEqual(self.login_request.status_code, 200)
-        result = json.loads(self.login_request.data)
+        self.assertEqual(self.login_response.status_code, 200)
+        result = json.loads(self.login_response.data)
         self.token = result['token']
 
     def test_manage_bucketlist_without_login(self):
-        self.bucketlist_request = self.app.post(
+        self.bucketlist_response = self.app.post(
             '/api/v1/bucketlists/', data={'name': 'BucketList1'})
-        self.assertEqual(self.bucketlist_request.status_code, 401)
+        self.assertEqual(self.bucketlist_response.status_code, 401)
 
     def create_bucketlist_item(self):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        self.bucketlist_item_request = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
+        self.bucketlist_item_response = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
                                                      data={
                                                          'name': 'I want to buy a Ferrarri'},
                                                      headers={'Token': self.token})
@@ -58,21 +58,21 @@ class TestBucketList(unittest.TestCase):
 
     def test_post_bucketlist_with_existing_name(self):
         self.create_bucketlist()
-        self.bucketlist_request = self.app.post('/api/v1/bucketlists/',
+        self.bucketlist_response = self.app.post('/api/v1/bucketlists/',
                                                data={'name': 'BucketList1'},
                                                headers={'Token': self.token})
-        self.assertEqual(self.bucketlist_request.status_code, 400)
-        result = json.loads(self.bucketlist_request.data)
+        self.assertEqual(self.bucketlist_response.status_code, 400)
+        result = json.loads(self.bucketlist_response.data)
         self.assertEqual(result['message'],
                          'Bucketlist already exist')
 
     def test_post_bucketlist_with_no_name(self):
         self.create_bucketlist()
-        self.bucketlist_request = self.app.post('/api/v1/bucketlists/',
+        self.bucketlist_response = self.app.post('/api/v1/bucketlists/',
                                                data={'name': ''},
                                                headers={'Token': self.token})
-        self.assertEqual(self.bucketlist_request.status_code, 400)
-        result = json.loads(self.bucketlist_request.data)
+        self.assertEqual(self.bucketlist_response.status_code, 400)
+        result = json.loads(self.bucketlist_response.data)
         self.assertEqual(result['message'],
                          'Please supply bucketlist name')
 
@@ -80,28 +80,28 @@ class TestBucketList(unittest.TestCase):
 
     def test_get_empty_bucketlist(self):
         self.login()
-        self.bucketlist_request = self.app.get('/api/v1/bucketlists/',
+        self.bucketlist_response = self.app.get('/api/v1/bucketlists/',
                                                headers={'Token': self.token})
-        self.assertEqual(self.bucketlist_request.status_code, 200)
-        result = json.loads(self.bucketlist_request.data)
+        self.assertEqual(self.bucketlist_response.status_code, 200)
+        result = json.loads(self.bucketlist_response.data)
         self.assertEqual(result['message'],
                          'Cannot locate any bucketlist')
 
     def test_get_all_bucketlist(self):
         self.create_bucketlist()
-        self.bucketlist_request = self.app.get('/api/v1/bucketlists/',
+        self.bucketlist_response = self.app.get('/api/v1/bucketlists/',
                                                headers={'Token': self.token})
-        result = json.loads(self.bucketlist_request.data)
+        result = json.loads(self.bucketlist_response.data)
         self.assertNotEqual(result, {})
 
     def test_put_bucketlist(self):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        self.bucketlist_request = self.app.put('/api/v1/bucketlists/{}'.format(bucketlist_id),
+        self.bucketlist_response = self.app.put('/api/v1/bucketlists/{}'.format(bucketlist_id),
                                                data={'name': 'BucketList2'},
                                                headers={'Token': self.token})
-        self.assertEqual(self.bucketlist_request.status_code, 200)
+        self.assertEqual(self.bucketlist_response.status_code, 200)
         bucketlist1 = BucketList.query.filter_by(name='BucketList1').first()
         bucketlist2 = BucketList.query.filter_by(name='BucketList2').first()
         self.assertIsNone(bucketlist1)
@@ -111,19 +111,19 @@ class TestBucketList(unittest.TestCase):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        self.bucketlist_request = self.app.put('/api/v1/bucketlists/{}'.format(bucketlist_id),
+        self.bucketlist_response = self.app.put('/api/v1/bucketlists/{}'.format(bucketlist_id),
                                                data={'name': 'BucketList1'},
                                                headers={'Token': self.token})
-        self.assertEqual(self.bucketlist_request.status_code, 406)
+        self.assertEqual(self.bucketlist_response.status_code, 406)
 
     def test_get_single_bucketlist(self):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        self.bucketlist_request = self.app.get('/api/v1/bucketlists/{}'.format(bucketlist_id),
+        self.bucketlist_response = self.app.get('/api/v1/bucketlists/{}'.format(bucketlist_id),
                                                data={'name': 'BucketList2'},
                                                headers={'Token': self.token})
-        result = json.loads(self.bucketlist_request.data)
+        result = json.loads(self.bucketlist_response.data)
         bucketlist_info = result['data']
         self.assertEqual(bucketlist_info['name'], 'BucketList1')
         self.assertEqual(bucketlist_info['items'], [])
@@ -132,17 +132,17 @@ class TestBucketList(unittest.TestCase):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        bucketlist_request = self.app.delete(
+        bucketlist_response = self.app.delete(
             '/api/v1/bucketlists/{}'.format(bucketlist_id), headers={'Token': self.token})
-        self.assertEqual(bucketlist_request.status_code, 204)
+        self.assertEqual(bucketlist_response.status_code, 204)
         bucketlist = BucketList.query.filter_by(id=bucketlist_id).first()
         self.assertIsNone(bucketlist)
 
     def test_delete_non_existing_bucketlist(self):
         self.create_bucketlist()
-        bucketlist_request = self.app.delete(
+        bucketlist_response = self.app.delete(
             '/api/v1/bucketlists/200', headers={'Token': self.token})
-        self.assertEqual(bucketlist_request.status_code, 400)
+        self.assertEqual(bucketlist_response.status_code, 400)
         
     def test_post_bucketlist_item(self):
         self.create_bucketlist_item()
@@ -152,18 +152,18 @@ class TestBucketList(unittest.TestCase):
         self.create_bucketlist_item()
         self.assertIsNotNone(self.bucketlist_item)
         item_id, bucketlist_id = self.bucketlist_item.id, self.bucketlist_item.bucketlist_id
-        self.bucketlist_item_request = self.app.get('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
+        self.bucketlist_item_response = self.app.get('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
                                                     headers={'Token': self.token})
-        self.assertIsNotNone(self.bucketlist_item_request.data)
+        self.assertIsNotNone(self.bucketlist_item_response.data)
 
     def test_get_empty_bucketlist_item(self):
         self.create_bucketlist()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        bucketlist_item_request = self.app.get('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
+        bucketlist_item_response = self.app.get('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
                                                headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 200)
-        result = json.loads(bucketlist_item_request.data)
+        self.assertEqual(bucketlist_item_response.status_code, 200)
+        result = json.loads(bucketlist_item_response.data)
         self.assertEqual(result['message'],
                          'Cannot locate any bucketlist items')
 
@@ -171,50 +171,50 @@ class TestBucketList(unittest.TestCase):
         self.create_bucketlist_item()
         bucketlist_id = BucketList.query.filter_by(
             name='BucketList1').first().id
-        bucketlist_item_request = self.app.get('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
+        bucketlist_item_response = self.app.get('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
                                                headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 200)
-        self.assertIsNotNone(bucketlist_item_request)
+        self.assertEqual(bucketlist_item_response.status_code, 200)
+        self.assertIsNotNone(bucketlist_item_response)
 
     def test_put_bucketlist_item(self):
         self.create_bucketlist_item()
         item_id, bucketlist_id = self.bucketlist_item.id, self.bucketlist_item.bucketlist_id
-        bucketlist_request = self.app.put('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
+        bucketlist_response = self.app.put('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
                                           data={
                                               'name': 'I want to buy a Sport Car', 'done': True},
                                           headers={'Token': self.token})
-        self.assertEqual(bucketlist_request.status_code, 200)
+        self.assertEqual(bucketlist_response.status_code, 200)
 
     def test_put_bucketlist_item_with_existing_name(self):
         self.create_bucketlist_item()
         item_id, bucketlist_id = self.bucketlist_item.id, self.bucketlist_item.bucketlist_id
-        bucketlist_item_request = self.app.put('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
+        bucketlist_item_response = self.app.put('/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id),
                                           data={
                                               'name': 'I want to buy a Ferrarri'},
                                           headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 400)
+        self.assertEqual(bucketlist_item_response.status_code, 400)
 
     def test_post_bucketlist_item_with_existing_name(self):
         self.create_bucketlist_item()
         bucketlist_id = self.bucketlist_item.bucketlist_id
-        bucketlist_item_request = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
+        bucketlist_item_response = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
                                           data={
                                               'name': 'I want to buy a Ferrarri'},
                                           headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 400)
-        result = json.loads(bucketlist_item_request.data)
+        self.assertEqual(bucketlist_item_response.status_code, 400)
+        result = json.loads(bucketlist_item_response.data)
         self.assertEqual(result['message'],
                          'Bucketlist item already exist')
 
     def test_post_bucketlist_item_with_no_name(self):
         self.create_bucketlist_item()
         bucketlist_id = self.bucketlist_item.bucketlist_id
-        bucketlist_item_request = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
+        bucketlist_item_response = self.app.post('/api/v1/bucketlists/{}/items/'.format(bucketlist_id),
                                           data={
                                               'name': ''},
                                           headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 400)
-        result = json.loads(bucketlist_item_request.data)
+        self.assertEqual(bucketlist_item_response.status_code, 400)
+        result = json.loads(bucketlist_item_response.data)
         self.assertEqual(result['message'],
                          'Please supply name for your bucketlist item')
 
@@ -222,17 +222,17 @@ class TestBucketList(unittest.TestCase):
     def test_delete_bucketlist_item(self):
         self.create_bucketlist_item()
         item_id, bucketlist_id = self.bucketlist_item.id, self.bucketlist_item.bucketlist_id
-        bucketlist_item_request = self.app.delete(
+        bucketlist_item_response = self.app.delete(
             '/api/v1/bucketlists/{}/items/{}'.format(bucketlist_id, item_id), headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 204)
+        self.assertEqual(bucketlist_item_response.status_code, 204)
         bucketlist_item = BucketListItem.query.filter_by(id=bucketlist_id).first()
         self.assertIsNone(bucketlist_item)
 
     def test_delete_non_existing_bucketlist_item(self):
         self.create_bucketlist_item()
-        bucketlist_item_request = self.app.delete(
+        bucketlist_item_response = self.app.delete(
             '/api/v1/bucketlists/{}/items/1000'.format(self.bucketlist_item.id), headers={'Token': self.token})
-        self.assertEqual(bucketlist_item_request.status_code, 400)
+        self.assertEqual(bucketlist_item_response.status_code, 400)
         
 
 if __name__ == '__main__':
